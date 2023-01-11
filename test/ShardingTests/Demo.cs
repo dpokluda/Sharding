6dd41dbe122a9fca3ac38cb4a32477a648f110ae
David Pokluda <david@pokluda.cz>
2023-01-10 21:27:28 -0800

    Adding simple demo code

---
+++ test/ShardingTests/Demo.cs
@@ -0,0 +1,77 @@
+﻿// -------------------------------------------------------------------------
+// <copyright file="Demo.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+// -------------------------------------------------------------------------
+
+using System.Diagnostics;
+using Sharding;
+
+namespace ShardingTests;
+
+[TestClass]
+public class Demo
+{
+    [TestMethod]
+    public void Run()
+    {
+        // create sharding component (with default 100 replicas per node)
+        var sharding = new ConsistentHashSharding();
+
+        // define nodes
+        var nodes = new List<string> { "Node-1", "Node-2", "Node-3", "Node-4", "Node-5" };
+        
+        // register sharding nodes
+        foreach (var node in nodes)
+        {
+            sharding.Add(node);
+        }
+        RunSteps(nodes, sharding);
+        
+        // let's remove one node
+        nodes.Remove("Node-3");
+        sharding.Remove("Node-3");
+        RunSteps(nodes, sharding);
+        
+        // add the node back in
+        nodes.Add("Node-3");
+        sharding.Add("Node-3");
+        RunSteps(nodes, sharding);
+        
+        // add a brand new node
+        nodes.Add("Node-6");
+        sharding.Add("Node-6");
+        RunSteps(nodes, sharding);
+    }
+
+    private static void RunSteps(List<string> nodes, ConsistentHashSharding sharding)
+    {
+        // sharding keys to existing nodes
+        var counts = new Dictionary<string, int>();
+        foreach (var node in nodes)
+        {
+            counts.Add(node, 0);
+        }
+
+        for (int i = 1; i < 100; i++)
+        {
+            var key = $"ValueKey-{i}";
+            var node = sharding.GetNode(key);
+            counts[node]++;
+        }
+
+        // display stats
+        Trace.WriteLine($"Stats for {nodes.Count} nodes:");
+        foreach (var node in nodes)
+        {
+            Trace.WriteLine($"{node}: {counts[node]}");
+        }
+
+        Trace.WriteLine("--------------------------");
+    }
+
+    private void RunStep()
+    {
+        
+    }
+}
